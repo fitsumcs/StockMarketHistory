cf6ebc7dd179c833737983d475f8db76ff73d583
Fitsum A <fitsumcs@gmail.com>
2021-04-06 23:56:03 +0400

    addoing the graph data of each company

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st 
+import yfinance as yf
 
 
 
@@ -14,5 +15,21 @@ st.title("""
 
 company_choice =  st.sidebar.selectbox("Company Selection", ("APPLE", "GOOGLE", "FACEBOOK"))
 st.sidebar.header("Date Range")
-st.sidebar.date_input('start date')
-st.sidebar.date_input('end date')
+start_date = st.sidebar.date_input('Start Date')
+end_date = st.sidebar.date_input('End Date')
+
+# Check the Choice 
+st.write(company_choice)
+st.write(start_date)
+st.write(end_date)
+
+# get the data of campany 
+company_data = yf.Ticker(company_choice)
+
+# Get Historical Data of the Company 
+trackDate = company_data.history(period='1d', start = start_date, end=end_date)
+
+
+# Draw the Graph 
+st.line_chart(trackDate.Close)
+st.line_chart(trackDate.Volume)
